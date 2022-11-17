5ced6d8eec81142d9a97df47b85018b820fb05b2
ShalihtaAlahakoon <it19960432@my.sliit.lk>
2022-11-17 08:52:06 +0530

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 import "./assets/css/App.css";
 import AddFile from './Components/AddFile';
 import SendMessage from './Components/Contact';
